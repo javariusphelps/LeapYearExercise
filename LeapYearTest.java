8077d4b01105f31e97f9286555074b58c344d3b4
Javarius Phelps <javariusphelps@gmail.com>
2020-06-01 19:38:11 -0400

    committing second changes

--- LeapYearTest.java
+++ LeapYearTest.java
@@ -1,3 +1,19 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
 public class LeapYearTest {
+
+    public void testLeapYear() {
+
+        @Test
+        LeapYear year = new LeapYear();
+
+        assertEquals(year.isLeapYear(1996));
+        assertEquals(year.isLeapYear(2001));
+        assertEquals(year.isLeapYear(2000));
+        assertEquals(year.isLeapYear(1900));
+
+    }
 
 }
